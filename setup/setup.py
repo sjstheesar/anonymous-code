3032e1b3fd41c57db6ca91ab86f39aebdf39bda8
anonymous <anonymous@abc.com>
2022-05-25 13:33:43 +0000

    code release

---
+++ setup/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="code_seq2seq",
+    author="anonymous2 anonymous2",
+    author_email="anonymous2@anonymous2.com",
+    license="MIT",
+    packages=["code_seq2seq"],
+)
